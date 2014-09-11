c848889f498aa1bbb3e5545a0a7cda2f1c7f7c33
Jonne Nauha <jonne@adminotech.com>
2014-09-11 18:09:44 +0300

    Fix mixed up cmake -G params.

--- CMake_VS2010_X64.bat
+++ CMake_VS2010_X64.bat
@@ -1,6 +1,6 @@
 @echo off
 IF EXIST tundra.sln del /Q tundra.sln
 cd tools\Windows\
-call RunCMake "Visual Studio 12 Win64"
+call RunCMake "Visual Studio 10 Win64"
 cd ..\..
 pause
--- CMake_VS2013.bat
+++ CMake_VS2013.bat
@@ -1,6 +1,6 @@
 @echo off
 IF EXIST tundra.sln del /Q tundra.sln
 cd tools\Windows\
-call RunCMake "Visual Studio 10"
+call RunCMake "Visual Studio 12"
 cd ..\..
 pause
